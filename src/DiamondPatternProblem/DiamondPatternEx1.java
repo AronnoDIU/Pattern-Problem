fd3e65496a7fcc6ed2b0ecb6c8926753e18dc244
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-08-22 07:04:03 +0600

    Diamond Pattern updated

--- src/DiamondPatternProblem/DiamondPatternEx1.java
+++ src/DiamondPatternProblem/DiamondPatternEx1.java
@@ -9,14 +9,89 @@ public class DiamondPatternEx1 {
 
         int Rows = scanner.nextInt();
 
+//        Upper Half Part
+
         for (int i = 0; i <= Rows; i++) {
-            for (int j = 0; j <= Rows - i; j++) {
+            for (int j = 0; j < Rows - i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 0; j < i; j++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+//        Lower Half Part
+
+        for (int i = Rows; i >= 0; i--) {
+            for (int j = 0; j < Rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+//        Another Way::
+
+        //        Upper Half Part
+
+        for (int i = 0; i <= Rows; i++) {
+            for (int j = 0; j < Rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= 2 * i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+//        Lower Half Part
+
+        for (int i = Rows; i >= 0; i--) {
+            for (int j = 0; j < Rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= 2 * i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
 }
+
+/*
+
+Expected Output::
+
+Enter your Rows number
+4
+    *
+   ***
+  *****
+ *******
+*********
+*********
+ *******
+  *****
+   ***
+    *
+    *
+   ***
+  *****
+ *******
+*********
+*********
+ *******
+  *****
+   ***
+    *
+
+* */
